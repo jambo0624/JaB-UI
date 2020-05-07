0ca637f19109e67e762c619d63d5b842845062e8
jambo <602975235@qq.com>
2020-05-07 15:59:36 +0800

    安装chai(a BDD / TDD assertion library),其中TDD主要是assert断言，BDD主要是should/expect.我们主要用到的是BDD。并用chai.expect添加4个测试用例，其中第一个vm.$mount()并没有挂载到具体元素，在内存中也可以。具体写法见vue文档测试用例部分

--- src/app.js
+++ src/app.js
@@ -15,3 +15,72 @@ new Vue({
     loading3: false
   }
 });
+
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'setting'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+
+  expect(href).to.eq('#j-setting')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      loading: true
+    }
+  })
+  vm.$mount(div)
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#j-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
