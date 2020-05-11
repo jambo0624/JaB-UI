d5a7091a73f8c4800260b4b2955ce3fbb9b777c7
jambo <602975235@qq.com>
2020-05-11 22:58:18 +0800

    加入row和col实现栅格功能，两个技巧是SCSS中@for $n from 1 through 24{#${n} {width:($n/24)*100}}和flex默认是nowrap，所以默认给50%，多了的话就会平均分布，50%就是至少有两个

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('j-button', Button);
 Vue.component('j-icon', Icon);
 Vue.component('j-button-group', ButtonGroup);
 Vue.component('j-input', Input);
+Vue.component('j-row', Row);
+Vue.component('j-col', Col);
 
 new Vue({
   el: '#app',
---
+++ src/row.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'JRow'
+  }
+</script>
+<style lang="scss" scoped>
+  .row{
+    display: flex;
+    flex-wrap: nowrap; /*flex默认属性*/
+  }
+</style>
