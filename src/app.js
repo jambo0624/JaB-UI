8b9dda50d5ced6b0f2d9972e5de19dfe24326947
jambo <602975235@qq.com>
2020-05-23 16:37:04 +0800

    layout基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Content from './content'
+import Footer from './footer'
+import Sider from './sider'
 
 Vue.component('j-button', Button);
 Vue.component('j-icon', Icon);
@@ -12,6 +17,11 @@ Vue.component('j-button-group', ButtonGroup);
 Vue.component('j-input', Input);
 Vue.component('j-row', Row);
 Vue.component('j-col', Col);
+Vue.component('j-layout', Layout);
+Vue.component('j-header', Header);
+Vue.component('j-content', Content);
+Vue.component('j-sider', Sider);
+Vue.component('j-footer', Footer);
 
 new Vue({
   el: '#app',
---
+++ src/footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="foot">
+    <slot></slot>
+  </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+  .foot{}
+</style>
