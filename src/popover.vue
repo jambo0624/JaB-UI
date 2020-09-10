d2fba15f184f44609bf5c0862898ab4c3d83889b
jambo <602975235@qq.com>
2020-09-10 15:02:44 +0800

    点击popover之外的部分，隐藏popover。涉及到了阻止冒泡，以及newtTick里面的事件绑定。为了content不被overflow-hidden隐藏，故将content调整到body之下，并对于css进行调整

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="j-popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="j-popover" @click.stop="xxx">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -18,7 +20,23 @@
     methods:{
       xxx(){
         this.visible = !this.visible
+        if(this.visible){
+          this.$nextTick(()=>{
+            document.body.appendChild(this.$refs.contentWrapper)
+            let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left+'px'
+            this.$refs.contentWrapper.style.top = top+'px'
+            let changeVisible = ()=>{
+              this.visible = false
+              document.removeEventListener('click',changeVisible)
+            }
+            document.addEventListener('click',changeVisible)
+          })
+        }
       }
+    },
+    mounted() {
+
     }
   }
 </script>
@@ -28,12 +46,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper{
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid #000;
-      box-shadow: 0 0 3px rgba(0,0,0,0.5);
-    }
+  }
+  .content-wrapper{
+    position: absolute;
+    border: 1px solid #000;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    transform: translateY(-100%);
   }
 </style>
